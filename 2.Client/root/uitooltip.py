918cc049af4eb492e4697187d4b54d6b8629d17f
Pipiasi Bogdan <bogdanpipiasi@yahoo.com>
2021-01-15 15:04:43 +0100

    Create uitooltip.py
    Tooltip error.

---
+++ 2.Client/root/uitooltip.py
@@ -0,0 +1,7 @@
+Search:
+	def AppendTextLine(self, text, color = FONT_COLOR, centerAlign = True):
+  
+Add abobe:
+	def SetThinBoardSize(self, width, height = 12) :
+		self.toolTipWidth = width 
+		self.toolTipHeight = height
