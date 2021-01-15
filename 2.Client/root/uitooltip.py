669567d6cac489d79c5620307bdddc70a9386b28
Pipiasi Bogdan <bogdanpipiasi@yahoo.com>
2021-01-15 15:07:16 +0100

    Update uitooltip.py

--- 2.Client/root/uitooltip.py
+++ 2.Client/root/uitooltip.py
@@ -1,7 +1,7 @@
-Search:
+# Search:
 	def AppendTextLine(self, text, color = FONT_COLOR, centerAlign = True):
   
-Add abobe:
+# Add abobe:
 	def SetThinBoardSize(self, width, height = 12) :
 		self.toolTipWidth = width 
 		self.toolTipHeight = height
